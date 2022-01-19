8a6f195ec22d46437253abdd81fd0d732e7318e6
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-01-20 04:50:23 +0900

    ✨ - Finally, I've done it!

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 mod report_handler;
 
 static DISCORD_BOT_TOKEN: once_cell::sync::Lazy<String> = once_cell::sync::Lazy::new(||
@@ -14,11 +16,54 @@ pub async fn run() {
     logger.info("Build Discord Client (Serenity)");
     let mut client = serenity::Client::builder(&*DISCORD_BOT_TOKEN)
         .framework(frame)
-        .event_handler(report_handler::CmdHandler)
         .raw_event_handler(report_handler::ReportHandler)
         .await.expect("Cannot create client.");
 
     if let Err(e) = client.start().await {
         logger.error(&format!("Cannot start client! {:?}", e))
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub(crate) struct TimeLine {
+    #[serde(rename(deserialize = "data"))]
+    data: Tweets,
+    #[allow(dead_code)]
+    #[serde(rename(deserialize = "meta"))]
+    meta: MetaData
+}
+
+#[derive(serde::Deserialize, Clone)]
+#[serde(transparent)]
+pub(crate) struct Tweets(Vec<Tweet>);
+
+#[derive(serde::Deserialize, Clone)]
+pub(crate) struct Tweet {
+    #[allow(dead_code)]
+    #[serde(rename(deserialize = "created_at"))]
+    created_at: DateTime<Utc>,
+    #[serde(rename(deserialize = "id"))]
+    id: String,
+    #[serde(rename(deserialize = "text"))]
+    text: String
+}
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize)]
+pub(crate) struct MetaData {
+    #[serde(rename(deserialize = "newest_id"))]
+    newest_id: String,
+    #[serde(rename(deserialize = "oldest_id"))]
+    oldest_id: String,
+    #[serde(rename(deserialize = "result_count"))]
+    result_count: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename(deserialize = "next_token"))]
+    next_token: Option<String>
+}
+
+impl TimeLine {
+    pub fn get_tweet(&self) -> Vec<Tweet> {
+        self.data.clone().0
     }
 }
--- src/service/report_handler.rs
+++ src/service/report_handler.rs
@@ -1,44 +1,55 @@
 use std::sync::Arc;
 use once_cell::sync::Lazy;
-use serenity::client::{Context, EventHandler, RawEventHandler};
+use reqwest::Client;
+use serenity::client::{Context, RawEventHandler};
 use serenity::http::Http;
-use serenity::model::channel::Message;
 use serenity::model::event::Event;
-use serenity::model::gateway::Ready;
+use serenity::model::gateway::Activity;
 use serenity::model::id::ChannelId;
 use tokio_cron_scheduler::{Job, JobScheduler};
 use crate::Logger;
+use crate::logger::Instant;
+use crate::service::TimeLine;
 
-pub(crate) struct CmdHandler;
 pub(crate) struct ReportHandler;
 
-const STILL_ALIVE: &str = "!still-alive";
-
+static HTTP_CLIENT: Lazy<Client> = Lazy::new(|| Client::new());
 static REPORT_CHANNEL: Lazy<ChannelId> = Lazy::new(|| {
     ChannelId(dotenv::var("REPORT_CHANNEL").ok()
         .and_then(|c| c.parse::<u64>().ok())
         .unwrap())
 });
-
-#[serenity::async_trait]
-impl EventHandler for CmdHandler {
-    async fn message(&self, ctx: Context, msg: Message) {
-
-    }
-
-    async fn ready(&self, ctx: Context, ready: Ready) {
-
-    }
-}
+static TWITTER_API: Lazy<String> = Lazy::new(|| {
+    dotenv::var("SURVEILLANCE_TARGET").ok()
+        .and_then(|s| Some(format!("https://api.twitter.com/2/tweets/search/recent?tweet.fields=created_at&query=from:{}", s)))
+        .unwrap_or(String::from("Najaran3"))
+});
+static TWITTER_API_TOKEN: Lazy<String> = Lazy::new(|| {
+    dotenv::var("BEARER_TOKEN").ok()
+        .unwrap()
+});
+static TWEET_URL: Lazy<String> = Lazy::new(|| {
+    dotenv::var("SURVEILLANCE_TARGET").ok()
+        .and_then(|s| Some(format!("https://twitter.com/{}/status/", s)))
+        .unwrap()
+});
 
 #[allow(unused_must_use)]
 #[serenity::async_trait]
 impl RawEventHandler for ReportHandler {
-    async fn raw_event(&self, ctx: Context, _ev: Event) {
+    async fn raw_event(&self, ctx: Context, ev: Event) {
+        match ev {
+            Event::Ready(a) => {
+                Instant::t_name("Login").out("Info", yansi::Color::Cyan, format!("Connected to {}", a.ready.user.name));
+                let a = Activity::listening("Waiting for Naja alive report...");
+                ctx.set_activity(a).await;
+            },
+            _ => ()
+        };
         let mut scd  = JobScheduler::new();
         let logger   = Logger::new(Some("Report"));
         let ctx_http = Arc::clone(&ctx.http);
-        scd.add(Job::new_async("0/5 * * * * * *", move |_uuid, _lock| {
+        scd.add(Job::new_async("0/30 * * * * * *", move |_uuid, _lock| {
             let ctx_http = ctx_http.clone();
             let logger   = logger.clone();
             Box::pin(async move {
@@ -52,7 +63,17 @@ impl RawEventHandler for ReportHandler {
 }
 
 async fn on_report(ctx_http: Arc<Http>, channel: ChannelId) {
-    if let Err(e) = channel.say(&ctx_http, "fire! 三:fire:").await {
-        println!("cannot send message! -- {:?}", e);
-    };
+    let client = &*HTTP_CLIENT;
+    let res = client.get(&*TWITTER_API)
+        .bearer_auth(&*TWITTER_API_TOKEN)
+        .send().await.unwrap()
+        .json::<TimeLine>()
+        .await.unwrap();
+    for tweet in res.get_tweet() {
+        if tweet.text.contains("#StillAliveNotify") {
+            if let Err(e) = channel.say(&ctx_http, format!("生存報告！{}{}", &*TWEET_URL, tweet.id)).await {
+                println!("cannot send message! -- {:?}", e);
+            };
+        }
+    }
 }
