3ff7459654681374c5325a18bd883247e6bcc15a
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-01-20 18:06:04 +0900

    🐛 - Fixed a bug that created infinite tasks. 🐛 + 📝 - Changed the scheduler process to fire in ReadyEvent.

--- src/service/report_handler.rs
+++ src/service/report_handler.rs
@@ -13,6 +13,8 @@ use crate::service::TimeLine;
 
 pub(crate) struct ReportHandler;
 
+const ALIVE_COMMAND: &str = "!alive";
+
 static HTTP_CLIENT: Lazy<Client> = Lazy::new(|| Client::new());
 static REPORT_CHANNEL: Lazy<ChannelId> = Lazy::new(|| {
     ChannelId(dotenv::var("REPORT_CHANNEL").ok()
@@ -38,27 +40,44 @@ static TWEET_URL: Lazy<String> = Lazy::new(|| {
 #[serenity::async_trait]
 impl RawEventHandler for ReportHandler {
     async fn raw_event(&self, ctx: Context, ev: Event) {
+        let ctx_http = Arc::clone(&ctx.http);
         match ev {
             Event::Ready(a) => {
                 Instant::t_name("Login").out("Info", yansi::Color::Cyan, format!("Connected to {}", a.ready.user.name));
-                let a = Activity::listening("Waiting for Naja alive report...");
-                ctx.set_activity(a).await;
+                let mut scd  = JobScheduler::new();
+                let logger   = Logger::new(Some("Report"));
+                Instant::t_name("Scheduler").out("Info", yansi::Color::Cyan, format!("Build Schedule Task."));
+                scd.add(Job::new_async("* 0/59 * * * * *", move |_uuid, _lock| {
+                    let ctx_http = ctx_http.clone();
+                    let logger   = logger.clone();
+                    Box::pin(async move {
+                        logger.info("fire!");
+                        let report = *REPORT_CHANNEL;
+                        on_report(ctx_http, report).await
+                    })
+                }).unwrap());
+                Instant::t_name("Scheduler").out("Info", yansi::Color::Cyan, format!("Built"));
+                tokio::spawn(scd.start());
+                return;
             },
-            _ => ()
+            Event::Resumed(r) => {
+                for trace in r.trace {
+                    Instant::t_name("Connect Resume")
+                        .out("Info", yansi::Color::Cyan, format!("Trace {}", trace.unwrap_or(String::from(""))));
+                }
+                return;
+            },
+            Event::MessageCreate(m) => {
+                let channel = *REPORT_CHANNEL;
+                if !m.message.author.bot && (m.message.content == ALIVE_COMMAND || m.message.channel_id == channel) {
+                    if let Err(e) = channel.say(&*ctx_http.clone(), ":white_check_mark: Alive").await {
+                        Instant::t_name("Cmd [Alive]").out("Error", yansi::Color::Red, "Cannot send cmd msg.")
+                    }
+                }
+                return;
+            },
+            _ => return ()
         };
-        let mut scd  = JobScheduler::new();
-        let logger   = Logger::new(Some("Report"));
-        let ctx_http = Arc::clone(&ctx.http);
-        scd.add(Job::new_async("0/30 * * * * * *", move |_uuid, _lock| {
-            let ctx_http = ctx_http.clone();
-            let logger   = logger.clone();
-            Box::pin(async move {
-                logger.info("fire!");
-                let report = *REPORT_CHANNEL;
-                on_report(ctx_http, report).await
-            })
-        }).unwrap());
-        scd.start().await;
     }
 }
 
@@ -72,7 +91,7 @@ async fn on_report(ctx_http: Arc<Http>, channel: ChannelId) {
     for tweet in res.get_tweet() {
         if tweet.text.contains("#StillAliveNotify") {
             if let Err(e) = channel.say(&ctx_http, format!("生存報告！{}{}", &*TWEET_URL, tweet.id)).await {
-                println!("cannot send message! -- {:?}", e);
+                println!("Cannot send message! -- {:?}", e);
             };
         }
     }
