7c1329cdf8801eff525ad158cb548a5337845edd
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-01-23 22:16:12 +0900

    🎨 - Organize the look of the code.

--- src/service/report_handler.rs
+++ src/service/report_handler.rs
@@ -1,15 +1,13 @@
 use std::sync::Arc;
-use chrono::{Datelike, DateTime, Duration, Timelike, TimeZone, Utc};
+use chrono::{DateTime, Duration, Utc};
 use once_cell::sync::Lazy;
 use reqwest::Client;
 use retainer::Cache;
 use serenity::client::{Context, RawEventHandler};
 use serenity::http::Http;
 use serenity::model::event::Event;
-use serenity::model::gateway::Activity;
 use serenity::model::id::ChannelId;
 use tokio_cron_scheduler::{Job, JobScheduler};
-use crate::Logger;
 use crate::logger::Instant;
 use crate::service::TimeLine;
 
@@ -86,7 +84,7 @@ impl RawEventHandler for ReportHandler {
                 let channel = *REPORT_CHANNEL;
                 if !m.message.author.bot && (m.message.content == ALIVE_COMMAND || m.message.channel_id == channel) {
                     if let Err(e) = channel.say(&*ctx_http.clone(), ":white_check_mark: Alive").await {
-                        Instant::t_name("Cmd [Alive]").out("Error", yansi::Color::Red, "Cannot send cmd msg.")
+                        Instant::t_name("Cmd [Alive]").out("Error", yansi::Color::Red, format!("Cannot send cmd msg.: {:?}", e))
                     }
                 }
                 return;
